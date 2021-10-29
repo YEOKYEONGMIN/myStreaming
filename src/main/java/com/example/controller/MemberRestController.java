a5ebc068716e1ff233825dbe3fc525ec48875f2a
byeongSoo <ubs4939@naver.com>
2021-10-30 03:18:07 +0900

    memberController 테스트용 코드 삭제

--- src/main/java/com/example/controller/MemberRestController.java
+++ src/main/java/com/example/controller/MemberRestController.java
@@ -29,19 +29,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/*")
 public class MemberRestController {
-	
-	// 테스트용
-    @GetMapping(value = "/one", produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
-    public MemberVO returnMember(){
-        MemberVO member = new MemberVO();
-        member.setId("testing");
-        member.setPasswd("testpass");
-        member.setName("테스트");
-        member.setEmail("test@naver.com");
 
-        return member;
-    }
-	
 	@Autowired
 	private MemberService memberService;
 	
