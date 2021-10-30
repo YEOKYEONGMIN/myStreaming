4d148142129c61cb0c9d3c6ae83175cd8046e7e9
xoni4 <xoni4@DESKTOP-2D9URGV>
2021-10-30 23:29:48 +0900

    bookmark Mapper, service , vo입니다

--- src/main/java/com/example/domain/BookmarkVO.java
+++ src/main/java/com/example/domain/BookmarkVO.java
@@ -4,7 +4,7 @@ import lombok.Data;
 
 @Data
 public class BookmarkVO {
-	
+	private String num; //북마크 번호
 	private String mid; // 북마크 작성자 아이디
 	private String name; // 북마크명
 	private String type; // 일단 Youtube는 'Y', 아프리카TV는 'A', 트위치는 'T'로 받아올 예정
