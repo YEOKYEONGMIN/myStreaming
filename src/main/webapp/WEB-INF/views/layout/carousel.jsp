c9d22932caba2cf7b743173e61aba38a3a3df8c2
yeoo <wotn128@naver.com>
2021-10-30 18:03:54 +0900

    캐러셀 수정

--- src/main/webapp/WEB-INF/views/layout/carousel.jsp
+++ src/main/webapp/WEB-INF/views/layout/carousel.jsp
@@ -2,43 +2,7 @@
     pageEncoding="UTF-8"%>
 <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
         <div class="carousel-inner">
-            <div class="carousel-item active">
-                <div class="cards-wrapper row">
-                    <div class="card">
-                        <img src="https://static-cdn.jtvnw.net/jtv_user_pictures/89e29e2e-f165-40e6-bc0c-d42205935216-profile_image-300x300.png"
-                            class="card-img-top" alt="...">
-                        <span class="live">생방송</span>
-                        <span class="viewer_count">시청자 9031명</span>
-                        <div class="card-body">
-                            <h5 class="card-title">침착맨</h5>
-                            <p class="card-text">침&펄 침착맨을 위한 지인과 여행 월드컵</p>
-                            <a href="#" id="btn_link">방송보러 가기</a>
-                        </div>
-                    </div>
-                    <div class="card">
-                        <img src="https://static-cdn.jtvnw.net/previews-ttv/live_user_runner0608-200x200.jpg"
-                            class="card-img-top" alt="...">
-                        <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk
-                                of
-                                the card's content.</p>
-                            <a href="#" id="btn_link">Go somewhere</a>
-                        </div>
-                    </div>
-                    <div class="card">
-                        <img src="https://static-cdn.jtvnw.net/previews-ttv/live_user_portialyn-200x200.jpg"
-                            class="card-img-top" alt="...">
-                        <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk
-                                of
-                                the card's content.</p>
-                            <a href="#" id="btn_link">Go somewhere</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            
         </div>
         <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
             <span class="carousel-control-prev-icon" aria-hidden="true"></span>
--- src/main/webapp/resources/js/index.js
+++ src/main/webapp/resources/js/index.js
@@ -13,55 +13,57 @@
 	        
 	        createCard(data);
 	        
-	        
-	        
 	    }
 	});
-	"https://static-cdn.jtvnw.net/previews-ttv/live_user_pjs9073-{width}x{height}.jpg"
+	
 	function createCard(data){
-		for(let i=0;i<20;i+3){	
+		for(let i=0;i<18;i+=3){	
 			let str = `
-				<div class="carousel-item active">
+				<div class="carousel-item">
 	            <div class="cards-wrapper">
 	                <div class="card">
-	                    <img src="${data[i].thumbnail_url}"
+	                    <img src="${data.data[i].thumbnail_url}"
 	                        class="card-img-top" alt="...">
 	                    <span class="live">생방송</span>
-	                    <span class="viewer_count">시청자 ${data[i].viewer_count}명</span>
+	                    <span class="viewer_count">시청자 ${data.data[i].viewer_count}명</span>
 	                    <div class="card-body">
-	                        <h5 class="card-title">침착맨</h5>
-	                        <p class="card-text">침&펄 침착맨을 위한 지인과 여행 월드컵</p>
-	                        <a href="#" id="btn_link">방송보러 가기</a>
-	                    </div>
-	                </div>
-	                
-	                <div class="card">
-	                    <img src="https://static-cdn.jtvnw.net/previews-ttv/live_user_runner0608-200x200.jpg"
-	                        class="card-img-top" alt="...">
-	                    <div class="card-body">
-	                        <h5 class="card-title">Card title</h5>
-	                        <p class="card-text">Some quick example text to build on the card title and make up the bulk
-	                            of
-	                            the card's content.</p>
-	                        <a href="#" id="btn_link">Go somewhere</a>
+	                        <h5 class="card-title">${data.data[i].user_name}</h5>
+	                        <p class="card-text">${data.data[i].title}</p>
+	                        <a href="https://www.twitch.tv/${data.data[i].user_login}" id="btn_link">방송보러 가기</a>
 	                    </div>
 	                </div>
 	                <div class="card">
-	                    <img src="https://static-cdn.jtvnw.net/previews-ttv/live_user_portialyn-200x200.jpg"
+	                    <img src="${data.data[i+1].thumbnail_url}"
 	                        class="card-img-top" alt="...">
+	                    <span class="live">생방송</span>
+	                    <span class="viewer_count">시청자 ${data.data[i+1].viewer_count}명</span>
 	                    <div class="card-body">
-	                        <h5 class="card-title">Card title</h5>
-	                        <p class="card-text">Some quick example text to build on the card title and make up the bulk
-	                            of
-	                            the card's content.</p>
-	                        <a href="#" id="btn_link">Go somewhere</a>
+	                        <h5 class="card-title">${data.data[i+1].user_name}</h5>
+	                        <p class="card-text">${data.data[i+1].title}</p>
+	                        <a href="https://www.twitch.tv/${data.data[i+1].user_login}" id="btn_link">방송보러 가기</a>
+	                    </div>
+	                </div>
+	                <div class="card">
+	                    <img src="${data.data[i+2].thumbnail_url}"
+	                        class="card-img-top" alt="...">
+	                    <span class="live">생방송</span>
+	                    <span class="viewer_count">시청자 ${data.data[i+2].viewer_count}명</span>
+	                    <div class="card-body">
+	                        <h5 class="card-title">${data.data[i+2].user_name}</h5>
+	                        <p class="card-text">${data.data[i+2].title}</p>
+	                        <a href="https://www.twitch.tv/${data.data[i+2].user_login}" id="btn_link">방송보러 가기</a>
 	                    </div>
 	                </div>
 	            </div>
 	        </div>
 			`
+			"https://static-cdn.jtvnw.net/previews-ttv/live_user_tmxk319-{width}x{height}.jpg"
+			let thumbnailWidth = str.replaceAll('{width}',300);
+			let thumbnailHeight = thumbnailWidth.replaceAll('{height}',300);
+			console.log(thumbnailHeight);
+			console.log($('.carousel-inner'));
+			$('.carousel-inner').append(str);
 		}
-		console.log(str);
 	}
 	
 	
