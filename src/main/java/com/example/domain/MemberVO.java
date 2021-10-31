b725a0e4a2eafadce35d97ff7f2e1faf05091f58
ansur <ansurtm@gmail.com>
2021-10-31 21:04:18 +0900

    회원 수정 화면 GET 수정, POST 추가

--- src/main/java/com/example/domain/MemberVO.java
+++ src/main/java/com/example/domain/MemberVO.java
@@ -12,4 +12,6 @@ public class MemberVO {
 	private String gender; // 성별
 	private String email; // 이메일
 	// String recv_email; // 이메일 수신여부 (딱히 필요성을 못느껴서 일단 보류)
+	
+	private ProfilepicVO profilepicVO;
 }
