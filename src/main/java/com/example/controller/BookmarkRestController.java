398d785d69c8b64a1e99fb0818eb3f4cefff1e4f
byeongSoo <ubs4939@naver.com>
2021-11-03 04:40:31 +0900

    충돌 수정 bookmarkValue @RequestBody 로 해결

--- src/main/java/com/example/controller/BookmarkRestController.java
+++ src/main/java/com/example/controller/BookmarkRestController.java
@@ -5,14 +5,12 @@ import java.util.Map;
 
 import javax.servlet.http.HttpSession;
 
+import com.example.domain.BookmarkVO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.example.service.BookmarkService;
 
@@ -24,13 +22,19 @@ public class BookmarkRestController {
 	private BookmarkService bookmarkService;
 	
 	
-	@PostMapping(value = "/bookmark/{bookmarkValue}",
+	@PostMapping(value = "/bookmark",
 			produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE })
-	public ResponseEntity<Map<String, Object>> addOrDeleteBookmark(@PathVariable("bookmarkValue") String bookmarkValue, 
+	public ResponseEntity<Map<String, Object>> addOrDeleteBookmark(@RequestBody BookmarkVO bookmarkVO,
 			HttpSession session) {
-		System.out.println(bookmarkValue);
+
 		String id = (String) session.getAttribute("id");
 		System.out.println(id);
+
+		System.out.println(bookmarkVO.getProfile_image_url());
+		System.out.println(bookmarkVO.getStreamerId());
+		System.out.println(bookmarkVO.getStreamerName());
+
+
 		Map<String, Object> map = new HashMap<String, Object>();
 		
 		map.put("id", id);
