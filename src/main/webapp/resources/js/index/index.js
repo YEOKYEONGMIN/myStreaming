7497ad5b77eb23fa4c04c4fb81bef00091bc2fe8
byeongSoo <ubs4939@naver.com>
2021-11-03 04:39:32 +0900

    충돌 수정

---
+++ src/main/webapp/resources/js/index/index.js
@@ -0,0 +1,8 @@
+$('.carousel').carousel({
+	interval: 0
+});
+
+document.write("<script src='/resources/js/index/caroTwitch.js'></script>");
+document.write("<script src='/resources/js/index/caroYoutube.js'></script>");
+
+
