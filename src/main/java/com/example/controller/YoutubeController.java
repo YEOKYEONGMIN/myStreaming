bdb854da0ed48d506921a15331330abd06e6d398
ansur <ansurtm@gmail.com>
2021-11-03 11:28:19 +0900

    Youtube 프로필사진 들고오는 api 추가

--- src/main/java/com/example/controller/YoutubeController.java
+++ src/main/java/com/example/controller/YoutubeController.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.http.*;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.HttpClientErrorException;
@@ -77,5 +78,58 @@ public class YoutubeController {
 
     } //getTwitchLiveList
 
+    
+    @GetMapping(value = "/channelId/{id}", produces = MediaType.APPLICATION_XML_VALUE)
+    public String getYoutubeProfilepic(@PathVariable("id") String id) {
+    	System.out.println("youtube profilepic 접근...");
+    	
+        HashMap<String, Object> result = new HashMap<String, Object>();
+
+        String jsonInString = "";
+
+        try {
+
+            HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory();
+            factory.setConnectTimeout(5000); //타임아웃 설정 5초
+            factory.setReadTimeout(5000);//타임아웃 설정 5초
+            RestTemplate restTemplate = new RestTemplate(factory);
+
+            HttpHeaders header = new HttpHeaders();
+            header.add("Accept","application/json");
+            HttpEntity<?> entity = new HttpEntity<>(header);
+
+            String channelId = id;
+            String apiKey = "AIzaSyAUs8F9TOZXZVz17UDKM8ojkbGtXCqTf8w";
+            StringBuilder url = new StringBuilder("https://www.googleapis.com/youtube/v3/channels?");
+            url.append("part=snippet");
+            url.append("&id=" + channelId);
+            url.append("&fields=items");
+            url.append("&key=" + apiKey);
+
+            UriComponents uri = UriComponentsBuilder.fromHttpUrl(url.toString()).build();
+            //이 한줄의 코드로 API를 호출해 MAP타입으로 전달 받는다.
+            ResponseEntity<Map> resultMap = restTemplate.exchange(uri.toString(), HttpMethod.GET, entity, Map.class);
+            result.put("statusCode", resultMap.getStatusCodeValue()); //http status code를 확인
+            result.put("header", resultMap.getHeaders()); //헤더 정보 확인
+            result.put("body", resultMap.getBody()); //실제 데이터 정보 확인
+
+            //데이터를 제대로 전달 받았는지 확인 string형태로 파싱해줌
+            ObjectMapper mapper = new ObjectMapper();
+            jsonInString = mapper.writeValueAsString(resultMap.getBody());
+
+        } catch (HttpClientErrorException | HttpServerErrorException e) {
+            result.put("statusCode", e.getRawStatusCode());
+            result.put("body"  , e.getStatusText());
+            System.out.println(e.toString());
+
+        } catch (Exception e) {
+            result.put("statusCode", "999");
+            result.put("body"  , "excpetion오류");
+            System.out.println(e.toString());
+        }
+
+        return jsonInString;
+    	
+    } // getYoutubeProfilepic
 
 }
