180dc3fbe36c5cba7d66c8b8a9d265e24582681a
ansur <ansurtm@gmail.com>
2021-11-09 00:03:49 +0900

    memberVO import빠진거 추가, 유튜브 컨트롤러 검색 메소드 추가

--- src/main/java/com/example/controller/YoutubeController.java
+++ src/main/java/com/example/controller/YoutubeController.java
@@ -183,5 +183,60 @@ public class YoutubeController {
 
         return jsonInString;
     } // getYoutubeChannelSubCount
+    
+    
+    // 채널명으로 검색
+    @GetMapping(value = "/search/{searchName}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public String getYoutubeSearch(@PathVariable("searchName") String searchName) {
+    	System.out.println("youtube search 접근...");
+    	
+        HashMap<String, Object> result = new HashMap<String, Object>();
+
+        String jsonInString = "";
+
+        try {
+
+            HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory();
+            factory.setConnectTimeout(5000); //타임아웃 설정 5초
+            factory.setReadTimeout(5000);//타임아웃 설정 5초
+            RestTemplate restTemplate = new RestTemplate(factory);
+
+            HttpHeaders header = new HttpHeaders();
+            header.add("Accept","application/json");
+            HttpEntity<?> entity = new HttpEntity<>(header);
+
+            String searchValue = searchName;
+            String apiKey = "AIzaSyAUs8F9TOZXZVz17UDKM8ojkbGtXCqTf8w";
+            StringBuilder url = new StringBuilder("https://www.googleapis.com/youtube/v3/search?");
+            url.append("part=snippet");
+            url.append("&type=channel");
+            url.append("&maxResults=20");
+            url.append("&q=" + searchValue);
+            url.append("&key=" + apiKey);
+
+            UriComponents uri = UriComponentsBuilder.fromHttpUrl(url.toString()).build();
+            //이 한줄의 코드로 API를 호출해 MAP타입으로 전달 받는다.
+            ResponseEntity<Map> resultMap = restTemplate.exchange(uri.toString(), HttpMethod.GET, entity, Map.class);
+            result.put("statusCode", resultMap.getStatusCodeValue()); //http status code를 확인
+            result.put("header", resultMap.getHeaders()); //헤더 정보 확인
+            result.put("body", resultMap.getBody()); //실제 데이터 정보 확인
+
+            //데이터를 제대로 전달 받았는지 확인 string형태로 파싱해줌
+            ObjectMapper mapper = new ObjectMapper();
+            jsonInString = mapper.writeValueAsString(resultMap.getBody());
+
+        } catch (HttpClientErrorException | HttpServerErrorException e) {
+            result.put("statusCode", e.getRawStatusCode());
+            result.put("body"  , e.getStatusText());
+            System.out.println(e.toString());
+
+        } catch (Exception e) {
+            result.put("statusCode", "999");
+            result.put("body"  , "excpetion오류");
+            System.out.println(e.toString());
+        }
+
+        return jsonInString;
+    } // getYoutubeSearch
 
 }
